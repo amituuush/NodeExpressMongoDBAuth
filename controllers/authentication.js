6d2a24297b358bffb822830ff6085dd3dc320e01
Amit Ranan <ranan.amit@gmail.com>
2016-12-11 15:41:41 -0800

    add logic to signup route

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -6,11 +6,25 @@ exports.signup = function(req, res, next) {
 
   // see if a user with a given email exists
   User.findOne({ email: email }, function(err, existingUser) {
-    
+    if (err) { return next(err); }
+
+    // if a user with email does exist, return an error
+    if (existingUser) {
+      return res.status(422).send({ error: 'Email is in use'});
+    }
+    // if a user with email does NOT exist, create and save user record
+    const user = new User({
+      email: email,
+      password: password
+    });
+
+    user.save(function(err) {
+      if (err) { return next(err); }
+
+      // respond to request indicating the user was created
+      res.json({ success: true });
+    }); // saves record to database
   });
-  // if a user with email does exist, return an error
 
-  // if a user with email does NOT exist, create and save user record
 
-  // respond to request indicating the user was created
 };
