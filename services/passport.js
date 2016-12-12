8c503b95864b3b93c16b6e376fefbd6411959fc7
Amit Ranan <ranan.amit@gmail.com>
2016-12-11 17:28:04 -0800

    connect jwt strategy to router, install passport-local

--- services/passport.js
+++ services/passport.js
@@ -4,10 +4,11 @@ const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 
+
 // set up options for JWT Strategy
 const jwtOptions = {
   jwtFromRequest: ExtractJwt.fromHeader('authorization'),
-  secretOrKey: config.secret;
+  secretOrKey: config.secret
 };
 
 // create JWT Strategy, function is called whenever user tries to log in or we need to authenticate a user with a jwt
@@ -24,7 +25,7 @@ const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
       done(null, false);
     }
 
-  })
+  });
 });
 
 // tell passport to use this strategy
